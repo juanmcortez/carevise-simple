0259b8f86abf92c324c4963de7aee4b8a6075bbc
juanmcortez <juanm.cortez@gmail.com>
2024-10-29 17:56:30 -0300

    🚧 Demographic phone initial stage

---
+++ database/factories/Commons/PhoneFactory.php
@@ -0,0 +1,23 @@
+<?php
+/*
+ * Copyright (c) 2024
+ *
+ *  @author Juan Manuel Cortéz <juanm.cortez@gmail.com>
+ *  @copyright 2024 Nobidium LLC.
+ *  @license MIT License
+ */
+
+namespace Database\Factories\Commons;
+
+use App\Models\Commons\Phone;
+use Illuminate\Database\Eloquent\Factories\Factory;
+
+class PhoneFactory extends Factory
+{
+    protected $model = Phone::class;
+
+    public function definition(): array
+    {
+        return [];
+    }
+}
