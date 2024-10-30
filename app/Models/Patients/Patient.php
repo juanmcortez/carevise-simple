3f2993163889992a75d94f933f10e54ad6527b73
juanmcortez <juanm.cortez@gmail.com>
2024-10-30 00:14:24 -0300

    🚧 Patient model

---
+++ app/Models/Patients/Patient.php
@@ -0,0 +1,68 @@
+<?php
+/*
+ * Copyright (c) 2024
+ *
+ *  @author Juan Manuel Cortéz <juanm.cortez@gmail.com>
+ *  @copyright 2024 Nobidium LLC.
+ *  @license MIT License
+ */
+
+namespace App\Models\Patients;
+
+use App\Models\Commons\Demographic;
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\SoftDeletes;
+use Illuminate\Database\Eloquent\Relations\HasOne;
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+
+class Patient extends Model
+{
+    use SoftDeletes, HasFactory;
+
+    /**
+     * The primary key associated with the table.
+     *
+     * @var string
+     */
+    protected $primaryKey = 'pid';
+
+    /**
+     * The relationships that should always be loaded.
+     *
+     * @var array
+     */
+    protected $with = ['demographic'];
+
+    /**
+     * The table associated with the model.
+     *
+     * @var string
+     */
+    protected $fillable = [
+        'demographic_id',
+    ];
+
+    /**
+     * The attributes that should be hidden for serialization.
+     *
+     * @var array<int, string>
+     */
+    protected $hidden = [
+        'id',
+        'demographic_id',
+        'created_at',
+        'updated_at',
+        'deleted_at',
+    ];
+
+    /**
+     * The demographic relationship associated with the model.
+     *
+     * @return HasOne
+     */
+    public function demographic(): HasOne
+    {
+        return $this->hasOne(Demographic::class, 'id', 'demographic_id')
+            ->withDefault();
+    }
+}
---
+++ database/factories/Patients/PatientFactory.php
@@ -0,0 +1,26 @@
+<?php
+/*
+ * Copyright (c) 2024
+ *
+ *  @author Juan Manuel Cortéz <juanm.cortez@gmail.com>
+ *  @copyright 2024 Nobidium LLC.
+ *  @license MIT License
+ */
+
+namespace Database\Factories\Patients;
+
+use App\Models\Patients\Patient;
+use App\Models\Commons\Demographic;
+use Illuminate\Database\Eloquent\Factories\Factory;
+
+class PatientFactory extends Factory
+{
+    protected $model = Patient::class;
+
+    public function definition(): array
+    {
+        return [
+            'demographic_id' => Demographic::factory(),
+        ];
+    }
+}
