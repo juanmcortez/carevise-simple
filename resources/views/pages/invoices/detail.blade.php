494105947114155823960427ad93a506dfcdc6db
juanmcortez <juanm.cortez@gmail.com>
2024-11-01 11:33:23 -0300

    🚧 Payments details per charge

--- resources/views/pages/invoices/detail.blade.php
+++ resources/views/pages/invoices/detail.blade.php
@@ -10,8 +10,15 @@
     @foreach($encounter->charges as $charge)
         {{ $charge }}<br />
         @if($charge->icds->count())
+            <br />
             @foreach($charge->icds as $icd)
                 {{ __('ICD: :code | :desc', ['code' => $icd->code, 'desc' => $icd->code_description]) }}<br />
+            @endforeach
+        @endif
+        @if($charge->payments->count())
+            <br />
+            @foreach($charge->payments as $payment)
+                {{ $payment }}<br />
             @endforeach
         @endif
         <br />
