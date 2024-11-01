34f9ef3a1301b4dc8cd3f03fc63dd499e23cf824
juanmcortez <juanm.cortez@gmail.com>
2024-11-01 17:41:42 -0300

    🚧 Name_slug access to the collection on the pages

--- resources/views/pages/patients/list.blade.php
+++ resources/views/pages/patients/list.blade.php
@@ -4,12 +4,17 @@
 @if($patients->isEmpty())
     {{ __('There are no patients available.') }}
 @else
+    <table>
     @foreach($patients as $patient)
-        <a href="{{ route('patients.detail', ['patient' => $patient->demographic->emailAddress->email]) }}" target="_self">
-            {{ $patient }}
-        </a>
-        <br />
+       <tr onclick="window.location='{{ route('patients.detail', ['patient' => $patient->demographic->name_slug]) }}'">
+           <td>{{ $patient->demographic->complete_name }}</td>
+           <td>{{ $patient->demographic->phone }}</td>
+           <td>{{ $patient->demographic->date_of_birth->format('M d, Y') }}</td>
+           <td>{{ $patient->pid }}</td>
+           <td></td>
+       </tr>
     @endforeach
+    </table>
     <br />
     <br />
     {!! $patients->links() !!}
